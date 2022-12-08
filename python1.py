0ea574a55a918f22d587f4536619844bc6bc4f83
Pornimams <porinma5730@gmail.com>
2022-12-08 17:48:51 +0530

    first commit

---
+++ python1.py
@@ -0,0 +1,16 @@
+p=120
+t=3
+r=2.5
+n=2
+while(True):
+    ch= int(input("Enter the Choice:"))
+    if ch== 1:  
+       asi=p*(1+ (r*t))
+       print("\n Simple Interest = ",asi)
+    
+    elif ch== 2:  
+        aci=p*((1+ r/n) ** (n*t))
+        print("\n Compound Interest = ",aci)
+        
+    else:
+        print("Incorrect")
